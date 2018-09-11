fa55d7467525902d81e15630ead2b54bcf93ab64
Vadim Nesterenko <vadim.nesterenko@saltpepper.ru>
2018-09-11 11:12:45 +0300

    Add controller

--- app/controllers/countries_controller.rb
+++ app/controllers/countries_controller.rb
@@ -2,18 +2,65 @@ class CountriesController < ApiController
 
   def index
     countries = CountryIndex.query(conditions)
+    # countries = countries.filter(term: { oon_status: true })
+    # countries = countries.query(range)
+    # countries = countries.order(capacity: :asc)
+    # countries = countries.query(match_param)
+    # countries = countries.query(existing)
+    # countries = countries.query(prefix)
+    countries = countries.load.to_a.map(&:attributes)
     render json: countries
   end
 
   private
 
   def conditions
-    return { match_all: {} } if query.blank?
-    { query_string: {"fields": ["title", "continent"], "query": "*#{query}*"} }
+    return { match_all: {} } if params[:query].blank?
+    query_condition 
+  end
+
+  def prefix
+    # { prefix: { title: {value: 'ит'} } }
+  end
+
+  def query_condition
+    { query_string: { fields: fields, query: query } }
+  end
+
+  def existing
+    { exists: { field: :capital } }
+  end
+
+  def match_param
+    { bool: {
+        must: {
+          match: {
+            "cities.title": "Сеул"
+          }
+        }
+      }
+    }
   end
 
   def query
-    params[:query]
+    "*#{params[:query]}*"
+  end
+
+  def fields
+    ["title", "continent", "cities.title", "cities.capacity"]
+  end
+
+  def range
+    { bool: {
+        filter: {
+          range: {
+            count_of_parties: {
+              lte: 3
+            }
+          }
+        }
+      }
+    }
   end
 end
 
