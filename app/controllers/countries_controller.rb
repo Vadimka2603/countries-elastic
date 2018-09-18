3dad1b281133a0ae827c14fe8dc5de9b67a346a6
Vadim Nesterenko <vadim.nesterenko@saltpepper.ru>
2018-09-18 11:04:59 +0300

    Add controller

--- app/controllers/countries_controller.rb
+++ app/controllers/countries_controller.rb
@@ -3,10 +3,10 @@ class CountriesController < ApiController
   def index
     countries = CountryIndex.query(conditions)
     # countries = countries.filter(term: { oon_status: true })
-    # countries = countries.query(range)
-    # countries = countries.order(capacity: :asc)
+    # countries = countries.query(range) # https://www.elastic.co/guide/en/elasticsearch/reference/current/query-dsl-range-query.html
+    # countries = countries.order(capacity: :desc)
     # countries = countries.query(match_param)
-    # countries = countries.query(existing)
+    # countries = countries.query(existing) # https://www.elastic.co/guide/en/elasticsearch/reference/current/query-dsl-exists-query.html
     # countries = countries.query(prefix)
     countries = countries.load.to_a.map(&:attributes)
     render json: countries
@@ -20,7 +20,7 @@ class CountriesController < ApiController
   end
 
   def prefix
-    # { prefix: { title: {value: 'ит'} } }
+    { prefix: { title: {value: 'ит'} } }
   end
 
   def query_condition
@@ -52,10 +52,10 @@ class CountriesController < ApiController
 
   def range
     { bool: {
-        filter: {
+        must: {
           range: {
-            count_of_parties: {
-              lte: 3
+            'count_of_parties'=> {
+              lte: 8
             }
           }
         }
